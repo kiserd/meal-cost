187075993a18f32ca2a98985c25c118e6588d7e7
kiserd <kiserd@oregonstate.edu>
2022-07-09 10:55:40 -0700

    cleaned up imports a bit

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,21 +1,7 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Header from './components/Header'
+// pages
 import Webpages from './pages/index'
 
 function App() {
-  const [getMessage, setGetMessage] = useState({})
-
-  useEffect(()=>{
-    axios.get('http://localhost:5000/dummy').then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log(error)
-    })
-
-  }, [])
   return (
     <div>
       <Webpages />
