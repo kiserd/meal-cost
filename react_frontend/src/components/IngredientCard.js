396f744b884332f75070b31434d41d25fa8b355b
kiserd <kiserd@oregonstate.edu>
2022-07-10 09:40:30 -0700

    updated ingredient card to include update and delete button placeholders

--- react_frontend/src/components/IngredientCard.js
+++ react_frontend/src/components/IngredientCard.js
@@ -1,20 +1,31 @@
+// library
 import React from 'react'
+// component
+import Button from './Button'
 
 const IngredientCard = ({ ingredient }) => {
   return (
-    <div className='uppercase bg-custom-card shadow-md max-w-md rounded-md text-custom-text-primary font-bold'>
-      <div className='p-2'>
-        <div className='text-custom-text-primary text-2xl'>
-            {ingredient.name}
+    <div className='max-w-md p-2 flex flex-row bg-custom-card shadow-md rounded'>
+      <div className='basis-3/4 flex flex-col uppercase'>
+          <div className='text-custom-text-primary text-2xl font-bold'>
+              {ingredient.name}
+          </div>
+          <div className='text-custom-text-secondary text-xs'>
+              {ingredient.category}
+          </div>
+          <div className='text-custom-text-secondary text-xs'>
+              ${ingredient.price} / {ingredient.unit}
+          </div>
+          <div className='text-custom-text-secondary text-xs'>
+              {ingredient.kcal} kcal / {ingredient.unit}
+          </div>
+      </div>
+      <div className='basis-1/4 flex flex-col content-end'>
+        <div className='basis-1/2'>
+          <Button text='Update' />
         </div>
-        <div className='text-custom-text-secondary text-xs'>
-            {ingredient.category}
-        </div>
-        <div className='text-custom-text-secondary text-xs'>
-            ${ingredient.price} / {ingredient.unit}
-        </div>
-        <div className='text-custom-text-secondary text-xs'>
-            {ingredient.kcal} kcal / {ingredient.unit}
+        <div className='basis-1/2'>
+          <Button text='Delete' />
         </div>
       </div>
     </div>
